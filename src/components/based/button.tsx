4a1a12cddfef3b4132fcbef489f0641314c9b537
JosephTseng <zxp930110@hotmail.com.tw>
2022-02-19 17:48:47 +0800

    create Button

---
+++ src/components/based/button.tsx
@@ -0,0 +1,14 @@
+import { styled } from "@material-ui/core/styles"
+
+const Button = styled("button")({
+    padding: "0.5em 1em",
+    border: "none",
+    borderRadius: "0.5em",
+    color: "white",
+    background: "grey",
+    cursor: "pointer",
+    whiteSpace: "nowrap",
+    textAlign: "center",
+})
+
+export default Button
