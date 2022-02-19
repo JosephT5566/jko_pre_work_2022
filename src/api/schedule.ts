56bbdf11303ea42ed55f444737ccc3064f182146
JosephTseng <zxp930110@hotmail.com.tw>
2022-02-19 19:35:38 +0800

    generate mock data randomly

--- src/api/schedule.ts
+++ src/api/schedule.ts
@@ -1,39 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import React, { useEffect, useState } from "react"
+import moment from "moment"
+import { v4 as uuidv4 } from "uuid"
 
-import { delay } from 'utils/helper';
-import { Competition } from 'type/schedule';
+import { delay } from "utils/helper"
+import { Competition } from "type/schedule"
 
 export function useGetScheduleByMonth(month: number) {
-	const [schedule, setSchedule] = useState<Competition[] | undefined>(
-		undefined
-	);
-	const [error, setError] = useState<Error | null>(null);
+    const [schedule, setSchedule] = useState<Competition[] | undefined>(undefined)
+    const [error, setError] = useState<Error | null>(null)
 
-	useEffect(() => {
-		const updateSchedule = async () => {
-			await delay(3000);
-			setSchedule(
-				mockSchedule.filter((com) => {
-					const date = moment(com.date);
-					return date.month() === month;
-				})
-			);
-		};
+    useEffect(() => {
+        const updateSchedule = async () => {
+            await delay(3000)
+            setSchedule(
+                mockSchedule.filter((com) => {
+                    const date = moment(com.date)
+                    return date.month() === month
+                })
+            )
+        }
 
-		setSchedule(undefined)
-		updateSchedule();
-	}, [month]);
+        setSchedule(undefined)
+        updateSchedule()
+    }, [month])
 
-	return { schedule, error };
+    return { schedule, error }
 }
 
+const mockTeams = [
+    { name: "新竹街口工程獅", icon: "/images/logo1.png" },
+    { name: "新竹街口工程獅", icon: "/images/logo2.png" },
+    { name: "新竹街口工程獅", icon: "/images/logo3.png" },
+    { name: "台北富邦勇士", icon: "/images/logo4.png" },
+]
+
 const mockSchedule: Competition[] = [
-	{
-		date: moment('2022-02-21').format(),
-		place: '桃園市立綜合體育館',
-		type: '例行賽',
-		home: { name: '新竹街口工程獅', icon: '/images/logo3.png' },
-		away: { name: '台北富邦勇士', icon: '/images/logo4.png' },
-	},
-];
+    {
+        gameId: uuidv4(),
+        date: moment("2022-02-21 13:30").format(),
+        place: "桃園市立綜合體育館",
+        type: "例行賽",
+        home: {...mockTeams[Math.floor(Math.random()*3)], score: 123 },
+        away: {...mockTeams[Math.floor(Math.random()*3)], score: 456 },
+    },
+]
