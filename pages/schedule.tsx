658fed05f8d26fe36f12f25bdd3a4ff3bbee13cf
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-02-20 18:45:39 +0800

    set click method for time table

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -3,15 +3,25 @@ import React, { useState } from "react"
 import TimeTable from "components/schedule/TimeTable"
 import TimeSelector from "components/schedule/TimeSelector"
 import { useGetScheduleByMonth } from "api/schedule"
+import { Competition } from "type/schedule"
 
 export default function Schedule() {
     const [month, setMonth] = useState(1)
     const { schedule } = useGetScheduleByMonth(month)
 
+    const handleClickPurchase = (com: Competition) => {
+        const { gameId, time, date } = com
+        console.log({ gameId, date, time })
+    }
+
     return (
         <>
             <TimeSelector month={month} setMonth={setMonth} />
-            {schedule ? <TimeTable schedule={schedule} /> : "loading..."}
+            {schedule ? (
+                <TimeTable schedule={schedule} onClickPurchase={handleClickPurchase} />
+            ) : (
+                "loading..."
+            )}
         </>
     )
 }
