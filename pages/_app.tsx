6bce2e6438240efd38a2175900a7ac10e695801f
Joseph Tseng <zxp930110@hotmail.com.tw>
2022-02-23 01:41:40 +0800

    fix styled-compoenet error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ThemeProvider, createTheme } from "@material-ui/core/styles"
@@ -5,6 +6,13 @@ import { ThemeProvider, createTheme } from "@material-ui/core/styles"
 const theme = createTheme()
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const jssStyles = document.querySelector("#jss-server-side")
+        if (jssStyles) {
+            jssStyles.parentElement!.removeChild(jssStyles)
+        }
+    }, [])
+
     return (
         <ThemeProvider theme={theme}>
             <Component {...pageProps} />
